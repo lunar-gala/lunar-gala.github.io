cdc7dc172d8c7840d00bb10cb572cfbb8f167875
mikinty <youmichaelc@gmail.com>
2020-09-12 00:31:11 -0400

    responsive bakcground

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
+/**
+ * Main script
+ */
+
 $(window).scroll(function() {
   var wScroll = $(window).scrollTop();
   if (wScroll < $(window).height()) {
-   opac = wScroll/400;
+    opac = wScroll/400;
     if (opac > .2) {opac = .2; }
     $(".overlay").css("opacity", 0.5 + opac);
   }
@@ -19,19 +23,20 @@ $(window).scroll(function() {
 
 
 window.onload = function() {
- $("path").addClass("stroke");
- $("text").addClass("fade");
- $(".background").addClass("active");
+  $("path").addClass("stroke");
+  $("text").addClass("fade");
+
+  $(".background").addClass("active");
 }
 
 
 
 if ($(window).width() > 800) {
-$(".hover-text").hover(function(){
-  var linkName = this.className;
-  var index = linkName.substr(linkName.length - 1);
+  $(".hover-text").hover(function(){
+    var linkName = this.className;
+    var index = linkName.substr(linkName.length - 1);
     $(".hover-img-" + index).css("opacity", 1);
   }, function() {
-  $(".hover-img").css("opacity", 0);
-});
+    $(".hover-img").css("opacity", 0);
+  });
 }
