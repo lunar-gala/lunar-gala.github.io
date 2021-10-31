800b8873fe4ae2671f20f045213ff9d1b45037cb
Bennett Huffman <bhuffman@andrew.cmu.edu>
2021-10-31 15:01:04 -0400

    removed unused html and css files

--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,6 @@
  * Main script
  */
 
-$(window).scroll(function() {
-  var wScroll = $(window).scrollTop();
-
-  // TODO: fix transparency scroll
-  /*
-  if (wScroll < $(window).height()) {
-    opac = wScroll/400;
-    if (opac > .2) {
-      opac = .2;
-    }
-
-    $(".background").css("opacity", 0.5 + opac);
-    $(".mobile-head-background").css("opacity", 0.5 + opac);
-  }
-  */
-  if (wScroll > $(window).height()) {
-    $(".freeze").addClass("fixed");
-  } else {
-    $(".freeze").removeClass("fixed");
-  }
-});
-
-
 window.onload = function() {
   $("path").addClass("stroke");
   $("text").addClass("fade");
@@ -32,8 +9,6 @@ window.onload = function() {
   $(".background").addClass("active");
   $(".mobile-head-background").addClass("active");
 }
-
-
 
 if ($(window).width() > 800) {
   $(".hover-text").hover(function(){
