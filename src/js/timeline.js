35d2c5fc9286d65e3508c622efacfb7c6d36b6fc
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-11-02 17:14:15 -0400

    setup base timeline scroll

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,6 +1,8 @@
 (async function() {
     const _this = this;
 
+    _this.$class = '.splide';
+
     // Use a Promise to wait for the DOM to finish loading.
     await new Promise((resolve, reject) => {
         document.addEventListener('DOMContentLoaded', resolve);
@@ -8,12 +10,42 @@
 
     _this.options = {
         direction: 'ttb',
+        type: 'ttb',
         height : '10rem',
         wheel : true,
         arrows: false,
-        pagination: false
+        pagination: false,
+        drag: true,
+        waitForTransition: false
     };
 
-    _this.timeline = new Splide('.splide', _this.options);
+    _this.$list = document.querySelector('.splide__list');
+
+    // Add TTY
+    (function () {
+        const element = document.createElement('div');
+        element.classList.add('splide__slide');
+
+        const image = document.createElement('img');
+        image.src = 'assets/img/timeline/TTY.jpg';
+
+        element.appendChild(image);
+        _this.$list.appendChild(element);
+    })();
+
+    _this.years = Array.from({ length: 2021 - 2000 }, (_, i) => i + 2000);
+    _this.years.forEach((year) => {
+        const element = document.createElement('div');
+        element.classList.add('splide__slide');
+        element.dataset.id = year;
+
+        const image = document.createElement('img');
+        image.src = `assets/img/timeline/${year}.jpg`;
+
+        element.appendChild(image);
+        _this.$list.appendChild(element);
+    });
+
+    _this.timeline = new Splide(_this.$class, _this.options);
     _this.timeline.mount();
 })();
