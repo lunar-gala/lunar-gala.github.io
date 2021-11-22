ef301259edb610076c7af19621cbba53a93c0d5f
Joseph <jxsephz@gmail.com>
2021-11-22 15:39:43 -0500

    add blur to nav bar on timeline page, improve preloader

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,6 +1,20 @@
+https://stackoverflow.com/questions/11071314/javascript-execute-after-all-images-have-loaded
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-    if ( document.URL.includes("index.html") ) {
+[].forEach.call( imgs, function( img ) {
+    if(img.complete)
+      incrementCounter();
+    else
+      img.addEventListener( 'load', incrementCounter, false );
+} );
+
+function incrementCounter() {
+    counter++;
+    if ( counter === len ) {
+        // console.log( 'All images loaded!' );
+            if ( document.URL.includes("index.html") ) {
         setTimeout(()=> {
             $(".loadingScreen").addClass("hideLoading");
         }, 1000);
@@ -16,6 +30,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     setTimeout(() => {
       $(".loadingScreen").remove();
     }, 3000);
-});
-
-  
+    }
+    
+}
