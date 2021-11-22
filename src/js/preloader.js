4c77c03e1909f2ffa469165435b6e418627b8311
Joseph <jxsephz@gmail.com>
2021-11-22 15:31:39 -0500

    additional visual tweaks

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,9 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    setTimeout(()=> {
-        $(".loadingScreen").addClass("hideLoading");
-    }, 1000);
-    
+    if ( document.URL.includes("index.html") ) {
+        setTimeout(()=> {
+            $(".loadingScreen").addClass("hideLoading");
+        }, 1000);
+        
+    }
+    else{
+        setTimeout(()=> {
+            $(".loadingScreen").addClass("hideLoading");
+        }, 0);
+    }
+
   
     setTimeout(() => {
       $(".loadingScreen").remove();
