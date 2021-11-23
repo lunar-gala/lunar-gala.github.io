a444fcb14ea1f10c7acb244078a0af954ea8c03f
Joseph <jxsephz@gmail.com>
2021-11-23 10:41:30 -0800

    change preloader logic

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,53 +1,19 @@
 
-// document.addEventListener("DOMContentLoaded", function(event) { 
-//     if ( document.URL.includes("index.html") ) {
-//         setTimeout(()=> {
-//             $(".loadingScreen").addClass("hideLoading");
-//         }, 1000);
+document.addEventListener("DOMContentLoaded", function(event) { 
+    if ( document.URL.includes("index.html") ) {
+        setTimeout(()=> {
+            $(".loadingScreen").addClass("hideLoading");
+        }, 1000);
         
-//     }
-//     else{
-//         setTimeout(()=> {
-//             $(".loadingScreen").addClass("hideLoading");
-//         }, 0);
-//     }
-
-  
-//     setTimeout(() => {
-//       $(".loadingScreen").remove();
-//     }, 3000);
-// });
-
-  
-var imgs = document.images,
-    len = imgs.length,
-    counter = 0;
-
-[].forEach.call( imgs, function( img ) {
-    if(img.complete)
-      incrementCounter();
-    else
-      img.addEventListener( 'load', incrementCounter, false );
-} );
-
-function incrementCounter() {
-    counter++;
-    if ( counter === len ) {
-
-        if ( document.URL.includes("index.html") ) {
-            setTimeout(()=> {
-                $(".loadingScreen").addClass("hideLoading");
-            }, 500);
-        }
-
-        else{
-            setTimeout(()=> {
-                $(".loadingScreen").addClass("hideLoading");
-            }, 0);
-        }
-    
-        setTimeout(() => {
-          $(".loadingScreen").remove();
-        }, 3000);
     }
-}
+    else{
+        $(".loadingScreen").addClass("hideLoading");
+    }
+
+  
+    setTimeout(() => {
+      $(".loadingScreen").remove();
+    }, 3000);
+});
+
+ 
